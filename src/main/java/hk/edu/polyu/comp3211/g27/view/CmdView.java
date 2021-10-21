03c9ae781a23d4dc4090516a2d8e3c2f6ed30b29
Carrot-Lou <75262237+Carrot-Lou@users.noreply.github.com>
2021-10-21 19:43:10 +0800

    Update CmdView.java

--- src/main/java/hk/edu/polyu/comp3211/g27/view/CmdView.java
+++ src/main/java/hk/edu/polyu/comp3211/g27/view/CmdView.java
@@ -10,27 +10,56 @@ public class CmdView {
      */
     
     
-    /*show the players left and their currrent money and propoerties*/
-    public List<Player> playersLeft(){
-        return List;
-    }
     
-    public int currentMoney(Player player) { 
-        return currentMoney; 
-    }
-    
-    public Square currentSquare(Player player) { 
-        return null; 
-    }
-    
-    public void move(Turn turn) {}
-    public void move(Player player, Square toSquare) {}
-    
-    
-    /*game board*/
+    /*start the game or continue*/
+    /*show the game manual*/
+    /*show the ID of the game*/
+    /*show the players left and their currrent money and properties*/
+    /*show the game board and the users' current place*/
+    /*show the turn of player that rolling dice ad the outcome*/
+    /*show the end or suspend of the game*/
     
     
     public String game() {
+        
+        public int start_game(){
+            return 0;
+        }
+        
+        public string Load_game(){
+            return "";
+        }
+        
+        public int manual(){
+            return 0;
+        }
+        
+        public string game_id(){
+            return "";
+        }
+        
+        public List<Player> playersLeft(){
+        return List;
+        }
+        
+        public int currentMoney(Player player) { 
+        return currentMoney; 
+        }
+    
+        public Square currentSquare(Player player) { 
+        return null; 
+        }
+    
+        public void move(Turn turn) {}
+        
+        public void move(Player player, Square toSquare) {}
+        
+        public void game_board(){}
+        
+        public string end_game(){
         return "";
+        }
+            
+        return "";
+        }
     }
-}
