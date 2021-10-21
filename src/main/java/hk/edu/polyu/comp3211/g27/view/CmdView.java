a09e1bbd60042e39695e0a7fac53db00f140e011
Carrot-Lou <75262237+Carrot-Lou@users.noreply.github.com>
2021-10-21 16:21:43 +0800

    Update CmdView.java

--- src/main/java/hk/edu/polyu/comp3211/g27/view/CmdView.java
+++ src/main/java/hk/edu/polyu/comp3211/g27/view/CmdView.java
@@ -8,6 +8,28 @@ public class CmdView {
      *
      * @return the String representation of the current game.
      */
+    
+    
+    /*show the players left and their currrent money and propoerties*/
+    public List<Player> playersLeft(){
+        return List;
+    }
+    
+    public int currentMoney(Player player) { 
+        return currentMoney; 
+    }
+    
+    public Square currentSquare(Player player) { 
+        return null; 
+    }
+    
+    public void move(Turn turn) {}
+    public void move(Player player, Square toSquare) {}
+    
+    
+    /*game board*/
+    
+    
     public String game() {
         return "";
     }
