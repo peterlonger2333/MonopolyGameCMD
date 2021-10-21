e1ccc1e076f416a4c99d9b30241b160619770cb7
Carrot-Lou <75262237+Carrot-Lou@users.noreply.github.com>
2021-10-21 20:13:23 +0800

    Update CmdView.java

--- src/main/java/hk/edu/polyu/comp3211/g27/view/CmdView.java
+++ src/main/java/hk/edu/polyu/comp3211/g27/view/CmdView.java
@@ -1,6 +1,6 @@
 package hk.edu.polyu.comp3211.g27.view;
 
-public class CmdView {
+public class cmdView {
     /**
      * The game includes:
      *  1. GameBoard
@@ -8,58 +8,63 @@ public class CmdView {
      *
      * @return the String representation of the current game.
      */
-    
-    
-    
+
+
+
     /*start the game or continue*/
     /*show the game manual*/
     /*show the ID of the game*/
-    /*show the players left and their currrent money and properties*/
+    /*show the players left and their current money and properties*/
     /*show the game board and the users' current place*/
     /*show the turn of player that rolling dice ad the outcome*/
     /*show the end or suspend of the game*/
     
-    
-    public String game() {
-        
-        public int start_game(){
-            return 0;
-        }
-        
-        public string Load_game(){
-            return "";
-        }
-        
-        public int manual(){
-            return 0;
-        }
-        
-        public string game_id(){
-            return "";
-        }
-        
-        public List<Player> playersLeft(){
-        return List;
-        }
-        
-        public int currentMoney(Player player) { 
-        return currentMoney; 
-        }
-    
-        public Square currentSquare(Player player) { 
-        return null; 
-        }
-    
-        public void move(Turn turn) {}
-        
-        public void move(Player player, Square toSquare) {}
-        
-        public void game_board(){}
-        
-        public string end_game(){
+    public String start_game(){
         return "";
-        }
-            
-        return "";
-        }
     }
+
+    public String Load_game(){
+        return "";
+    }
+
+    public String manual(){
+        return "";
+    }
+
+    public String game_id(){
+        return "";
+    }
+
+    public String playersLeft(){
+        return "";
+    }
+
+    public String currentMoney() {
+        return "";
+    }
+
+    public String currentProperties(){
+        return "";
+    }
+
+    public String currentSquare() {
+        return "";
+    }
+
+    public String turn() {
+        return "";
+    }
+
+    public String move() {
+        return "";
+    }
+
+    public String game_board(){
+        return "";
+    }
+
+    public String end_game(){
+        return "";
+    }
+}
+
