697868b593c9a6418acce5822611b0f9af6575db
Jonathan Lee <56100834+peterlonger2333@users.noreply.github.com>
2021-10-22 12:28:17 +0800

    Update GoSquare.java

--- src/main/java/hk/edu/polyu/comp3211/g27/model/square/GoSquare.java
+++ src/main/java/hk/edu/polyu/comp3211/g27/model/square/GoSquare.java
@@ -4,11 +4,13 @@ import hk.edu.polyu.comp3211.g27.model.Game;
 import hk.edu.polyu.comp3211.g27.model.Player;
 
 public class GoSquare extends Square{
-    public static final int SALARY = 1500;
+    private static final int SALARY = 1500;
 
     public GoSquare(int index, String label){
         super(index, label);
     }
+    
+    public int getSalary(){return SALARY;}
 
     /**
      * Each time a player lands on or passes by Go square, the player will get HKD 1500 salary.
