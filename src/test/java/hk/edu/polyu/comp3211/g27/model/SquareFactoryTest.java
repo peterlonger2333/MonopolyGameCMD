7ea93ab331f90418eec47dd0534173f88fa13c01
McXD <fyl155165@gmail.com>
2021-11-01 12:38:09 +0800

    Add SquareFactory test class

---
+++ src/test/java/hk/edu/polyu/comp3211/g27/model/SquareFactoryTest.java
@@ -0,0 +1,7 @@
+package hk.edu.polyu.comp3211.g27.model;
+
+/**
+ * Test that the {@link SquareFactory} fetches the correct squares according to our game board
+ */
+public class SquareFactoryTest {
+}
