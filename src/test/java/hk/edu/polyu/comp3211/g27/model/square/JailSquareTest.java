b92684c4b475af015f58efdb9ad136dd1edb0f89
unknown <19087103d@connect.polyu.hk>
2021-11-03 21:01:28 +0800

    Implement square tests

--- src/test/java/hk/edu/polyu/comp3211/g27/model/square/JailSquareTest.java
+++ src/test/java/hk/edu/polyu/comp3211/g27/model/square/JailSquareTest.java
@@ -3,11 +3,16 @@ package hk.edu.polyu.comp3211.g27.model.square;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.verify;
+
 // Tests for already-in-jail scenario should be done in GameTest
 public class JailSquareTest extends SquareTestBase {
+    private int squareIndex=6;
+    private String squareLabel = "IN JAIL";
     @Test
     @DisplayName("Should have no effect when player directly lands on this square")
     public void shouldHaveNoEffect() {
-
+        JailSquare jailSquare = new JailSquare(squareIndex,squareLabel);
+        verify(jailSquare).onEnter(game);
     }
 }
