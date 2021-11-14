0dd778da6876aebcf23211dd5a114fcc0c713951
Jonathan Lee <56100834+peterlonger2333@users.noreply.github.com>
2021-11-14 16:32:30 +0800

    Update SquareFactory.java

--- src/main/java/hk/edu/polyu/comp3211/g27/model/SquareFactory.java
+++ src/main/java/hk/edu/polyu/comp3211/g27/model/SquareFactory.java
@@ -1,9 +1,11 @@
 package hk.edu.polyu.comp3211.g27.model;
 
 import hk.edu.polyu.comp3211.g27.model.square.*;
+import org.checkerframework.checker.units.qual.C;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import java.util.HashMap;
 
 /**
  * A convenient and efficient place to store and fetch {@link Square}s in a game board.
@@ -11,16 +13,72 @@ import javax.annotation.Nullable;
 public class SquareFactory {
     public static final int SQUARE_CNT = 20; // number of squares in the game
 
-    public static @Nullable Square getSquare(int index) {
-        return null;
-    }
+    private static HashMap<Integer, String> squareList= new HashMap<Integer, String>(){
+        {
+            put(2,"Central");
+            put(3,"Wan Chai");
+            put(5,"Stanley");
+            put(7,"Shek O");
+            put(8,"Mong Kok");
+            put(9,"CHANCE");
+            put(10,"Tsing Yi");
+            put(12,"Shatin");
+            put(13,"CHANCE");
+            put(14,"Tuen Mun");
+            put(15,"Tai Po");
+            put(17,"Sai Kung");
+            put(18,"Yuen Long");
+            put(19,"CHANCE");
+            put(20,"Tai O");
+        }
+    };
+
+    private static HashMap<Integer, Integer> priceList = new HashMap<Integer, Integer>(){
+        {
+            put(2,800);
+            put(3,700);
+            put(5,600);
+            put(7,400);
+            put(8,500);
+            put(10,400);
+            put(12,700);
+            put(14,400);
+            put(15,500);
+            put(17,400);
+            put(18,400);
+            put(20,600);
+        }
+    };
+
+    private static HashMap<Integer, Integer> rentList = new HashMap<Integer, Integer>(){
+        {
+            put(2,90);
+            put(3,65);
+            put(5,60);
+            put(7,10);
+            put(8,40);
+            put(10,15);
+            put(12,75);
+            put(14,20);
+            put(15,25);
+            put(17,10);
+            put(18,25);
+            put(20,25);
+        }
+    };
+
+    public static @Nullable Square getSquare(int index) {return null;}
 
     public static @Nullable Square getSquare(String label) {
         return null;
     }
 
+    public static @Nullable FreeParkingSquare getFreeParkingSquare(){
+        return new FreeParkingSquare(11,"FREE PARKING");
+    }
+
     public static @Nullable PropertySquare getPropertySquare(int index) {
-        return null;
+        return new PropertySquare(index, squareList.get(index), priceList.get(index), rentList.get(index));
     }
 
     /**
@@ -30,24 +88,33 @@ public class SquareFactory {
      * @return the property square with label
      */
     public static @Nullable PropertySquare getPropertySquare(String label) {
-        return null;
+        int index = 0;
+        for (int key: squareList.keySet()){
+            if (squareList.get(key) == label){
+                index = key;
+                break;
+            }
+        }
+        return new PropertySquare(index, label, priceList.get(index), rentList.get(index));
     }
 
     public static ChanceSquare getChanceSquare(int index) {
-        return null;
+        return new ChanceSquare(index, squareList.get(index));
     }
 
     public static @NotNull GoSquare getGoSquare() {
-        return null;
+        return new GoSquare(1, "GO");
     }
 
     public static @NotNull GoToJailSquare getGoToJailSquare() {
-        return null;
+        return new GoToJailSquare(16, "GO TO JAIL");
     }
 
     public static @NotNull IncomeTaxSquare getIncomeTaxSquare() {
-        return null;
+        return new IncomeTaxSquare(4, "INCOME TAX");
     }
 
-    public static @NotNull JailSquare getJailSquare() {return null;}
+    public static @NotNull JailSquare getJailSquare() {
+        return new JailSquare(6, "JAIL");
+    }
 }
