dea32e7cf1a54a68207a6b0533ad759b8c886c37
Jonathan Lee <56100834+peterlonger2333@users.noreply.github.com>
2021-11-15 12:58:39 +0800

    Update SquareFactoryTest.java

--- src/test/java/hk/edu/polyu/comp3211/g27/model/SquareFactoryTest.java
+++ src/test/java/hk/edu/polyu/comp3211/g27/model/SquareFactoryTest.java
@@ -11,13 +11,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class SquareFactoryTest {
 
-    private static final int freeParkingSquareIndex = 1;
+    private static final int[] squareIndex = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    private static final String[] squareLabel = {"GO","Central","Wan Chai","INCOME TAX",
+            "Stanley","IN JAIL","Shek O","Mong Kok","CHANCE",
+            "Tsing Yi","FREE PARKING","Shatin","CHANCE","Tuen Mun",
+            "Tai Po","GO TO JAIL","Sai Kung","Yuen Long","CHANCE","Tai O"};
+
+    private static final int freeParkingSquareIndex = 11;
     private static final String freeParkingSquareLabel = "FREE PARKING";
 
     private static final int[] propertySquareIndex = {2, 3, 5, 7, 8, 10, 12, 14, 15, 17, 18, 20};
     private static final String[] propertySquareLabel = {"Central", "Wan Chai", "Stanley",
-                                                         "Shek O", "Mong Kok", "Tsing Yi", "Shatin",
-                                                         "Tuen Mun", "Tai Po", "Sai Kung", "Yuen Long", "Tai O"};
+            "Shek O", "Mong Kok", "Tsing Yi", "Shatin",
+            "Tuen Mun", "Tai Po", "Sai Kung", "Yuen Long", "Tai O"};
+    private static final int[] propertySquarePrice = {800,700,600,400,500,400,700,400,500,400,400,600};
+    private static final int[] propertySquareRent = {90,65,60,10,40,15,75,20,25,10,25,25};
 
     private static final int[] chanceSquareIndex = new int[]{9, 13, 19};
     private static final String chanceSquareLabel = "CHANCE";
@@ -35,20 +43,45 @@ public class SquareFactoryTest {
     private static final String jailSquareLabel = "IN JAIL";
 
     @Test
+    @DisplayName("Fetch square test.")
+    public void fetchSquareTest(){
+        for(int i = 0; i < squareIndex.length; i++){
+            Square square = SquareFactory.getSquare(squareIndex[i]-1);
+
+            assertEquals(squareLabel[i], square.getLabel());
+        }
+    }
+
+    @Test
     @DisplayName("Fetch free parking square test.")
     public void fetchFreeParkingSquareTest(){
         FreeParkingSquare freeParkingSquare = SquareFactory.getFreeParkingSquare();
 
+        assertEquals(freeParkingSquareIndex, freeParkingSquare.getIndex());
         assertEquals(freeParkingSquareLabel, freeParkingSquare.getLabel());
     }
 
     @Test
-    @DisplayName("Fetch property square test.")
-    public void fetchPropertySquareTest(){
+    @DisplayName("Fetch property square test by index.")
+    public void fetchPropertySquareByIndexTest(){
         for (int i = 0; i < propertySquareIndex.length; i++){
             PropertySquare propertySquare = SquareFactory.getPropertySquare(propertySquareIndex[i]-1);
 
             assertEquals(propertySquareLabel[i], propertySquare.getLabel());
+            assertEquals(propertySquarePrice[i], propertySquare.getPrice());
+            assertEquals(propertySquareRent[i], propertySquare.getRent());
+        }
+    }
+
+    @Test
+    @DisplayName("Fetch property square test by label.")
+    public void fetchPropertySquareByLabelTest(){
+        for (int i = 0; i < propertySquareLabel.length; i++){
+            PropertySquare propertySquare = SquareFactory.getPropertySquare(propertySquareLabel[i]);
+
+            assertEquals(propertySquareIndex[i], propertySquare.getIndex());
+            assertEquals(propertySquarePrice[i], propertySquare.getPrice());
+            assertEquals(propertySquareRent[i], propertySquare.getRent());
         }
     }
 
