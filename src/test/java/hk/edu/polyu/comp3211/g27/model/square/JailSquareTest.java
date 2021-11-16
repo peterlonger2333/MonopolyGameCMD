0b482cfa3519f73f79c304fe46f5a8a8edc1090f
win10 <becauseimbatman2333@gmail.com>
2021-11-17 00:23:17 +0800

    improve jail square test

--- src/test/java/hk/edu/polyu/comp3211/g27/model/square/JailSquareTest.java
+++ src/test/java/hk/edu/polyu/comp3211/g27/model/square/JailSquareTest.java
@@ -1,11 +1,17 @@
 package hk.edu.polyu.comp3211.g27.model.square;
 
+import hk.edu.polyu.comp3211.g27.model.Dices;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 // Tests for already-in-jail scenario should be done in GameTest
 public class JailSquareTest extends SquareTestBase {
@@ -28,4 +34,20 @@ public class JailSquareTest extends SquareTestBase {
     }
 
     //TODO: test onPrisonerTurn()
+
+    @Test
+    @DisplayName("Player can choose to pay fine")
+    public void canPayFine() throws IOException {
+        InputStream mockInputStream = new ByteArrayInputStream("y\n".getBytes(StandardCharsets.UTF_8));
+        System.setIn(mockInputStream);
+
+        when(game.currentTurn()).thenReturn(turn);
+        when(game.currentMoney(player1)).thenReturn(150);
+
+        game.putInJail(player1);
+
+        jailSquare.onPrisonerTurn(game);
+
+        verify(game, times(1)).subtractMoney(150, player1); // check that the money is paid
+    }
 }
